24c5c6a76b8ca1e36a8ac23c9e62bf65889b7011
Helard Flores <helard@gmail.com>
2023-02-06 20:17:25 -0500

    se agrego el scope y el listener para guardar los datos del tenant en la session

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Scopes\TenantScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -41,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new TenantScope);
+    }
 }
